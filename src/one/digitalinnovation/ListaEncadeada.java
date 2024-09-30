dcbccd5c128a8f7168e3b83cdbd73afdf2831dab
arymariano <ary.vm@hotmail.com>
2024-09-30 15:51:36 -0300

    lista encadeada

---
+++ src/one/digitalinnovation/ListaEncadeada.java
@@ -0,0 +1,77 @@
+package one.digitalinnovation;
+
+public class ListaEncadeada<T> {
+
+    No<T> referenciaEntrada;
+
+    public ListaEncadeada(){
+        this.referenciaEntrada = null;
+    }
+    
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+
+        if (this.isEmpty()){
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+
+        for(int i = 0; i< this.size()-1; i++){
+            noAuxiliar = noAuxiliar.getNoRef();
+        }
+        noAuxiliar.setNoRef(novoNo);
+    }
+
+    public No<T> getNo(int index){
+        validaIndice(index);
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+        
+
+        for(int i = 0; i< this.size()-1; i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getNoRef();
+        }
+
+        return noRetorno;
+
+    }
+
+    public int size(){
+        int tamanhoLista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+
+        while(true){
+            if(referenciaAux != null){
+                tamanhoLista ++;
+                if(referenciaAux.getNoRef() != null){
+                    referenciaAux = referenciaAux.getNoRef();
+                }
+                else{
+                    break;
+                }
+            }
+            else{
+                break;
+            }
+        }
+        return tamanhoLista;
+    }
+
+    private void validaIndice(int index){
+
+        if(index >= size()){
+            int ultimoIndex= this.size() -1;
+            throw new IndexOutOfBoundsException("Não existe conteúdo no índice: "+ index +"\n a Lista só vai até o índice : " + ultimoIndex);
+        }
+
+    }
+
+
+    public boolean isEmpty(){
+        return referenciaEntrada == null? true : false;
+    }
+
+}
---
+++ src/one/digitalinnovation/No.java
@@ -0,0 +1,55 @@
+package one.digitalinnovation;
+
+public class No<T> {
+
+    private T conteudo;
+    private No noRef;
+
+
+    public No(){
+
+    }
+
+    public No(T conteudo) {
+        this.conteudo = conteudo;
+        this.noRef = null;
+    }
+
+    public No(T conteudo, No noRef) {
+        this.conteudo = conteudo;
+        this.noRef = noRef;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No<T> getNoRef() {
+        return noRef;
+    }
+
+    public void setNoRef(No noRef) {
+        this.noRef = noRef;
+    }
+
+    @Override
+    public String toString() {
+        return "No [conteudo=" + conteudo + "]";
+    }
+
+    public String toStringEncadeado(){
+        String str =  "No [conteudo=" + conteudo + "]";
+        if(noRef != null){
+            str += "->" + noRef.toString(); 
+        }
+        else{
+            str+= "->null";
+        }
+        return str;
+    }
+
+}
